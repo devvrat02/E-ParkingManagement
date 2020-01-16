604ac2a8a9388e099bba3afa2e36c8bd4937c470
devvrat02 <34860774+devvrat02@users.noreply.github.com>
2020-01-16 13:51:50 +0530

    update cancel button
    user can cancel the booking slot

--- status.php
+++ status.php
@@ -1,27 +1,31 @@
 <?php
 session_start();
 require 'mysqlConnect.php';
+
 $a=$_SESSION["emal"];
 $b=$_SESSION["psd"];
+
 $sql="select * from parkingbook where email='$a'";
 $result=$con->query($sql);
 if($result->num_rows>0)
 {
-	echo "<table border='1'><tr><th>Email_ID</th><th>Car_Number</th><th>Slot</th><th>Rate</th><th>Entry time</th><th>Exit_Time</th></tr>";
+	echo "<form method='post' action='cancel.php' name='frm'><table border='1'><tr><th>Email_ID</th><th>Car_Number</th><th>Slot</th><th>Rate</th><th>Entry time</th><th>Exit_Time</th></tr>";
 	while($row=$result->fetch_assoc())
 	{
-		echo "<tr><td>".$row["email"]."</td>";
+		$em=$row["email"];
+		$pd=$row["password"];
+		echo "<input type='hidden' name='email' value='$em'><input type='hidden' name='password' value='$pd'><tr><td>".$row["email"]."</td>";
 		echo "<td>".$row["car_number"]."</td>";
 		echo "<td>".$row["slots"]."</td>";
 		echo "<td>".$row["rates"]."</td>";
 		echo "<td>".$row["entry"]."</td>";
 		echo "<td>".$row["exittime"]."</td></tr>";
 	}
-	echo "</table>";
-}
-else{
-	echo "<h5>Parking Not Book</h5>";
+	echo "<input type='submit' value='cancel'></form></table>";
 }
 
+else{
+	echo "<h6>You Are not Book Your Parking Slot Yet</h6>";
+}
 
 ?>
