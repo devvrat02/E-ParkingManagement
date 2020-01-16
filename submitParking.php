25df05e5c0cd1ae58215d20b74467f2971a82568
devvrat02 <34860774+devvrat02@users.noreply.github.com>
2020-01-16 13:56:30 +0530

    update
    Insert query changes for table updation

--- submitParking.php
+++ submitParking.php
@@ -8,12 +8,13 @@ $d=$_POST["entry"];
 $e=$_POST["exit"];
 $f=$_POST["slot"];
 $g=$_POST["rate"];
+$check=$_SESSION['page'];
 $sql="select * from parkingbook where email='$a' ";
 $result=$con->query($sql);
 if($result->num_rows==0)
 {
 
-$sql="insert into parkingbook(email,password,car_number,entry,exittime,slots,rates) values('$a','$b','$c','$d','$e','$f','$g')";
+$sql="insert into parkingbook(email,password,car_number,entry,exittime,slots,rates,parking) values('$a','$b','$c','$d','$e','$f','$g','$check')";
 				if($con->query($sql)===true)
 				{
 					echo "Data inserted successfilly";
@@ -21,8 +22,8 @@ $sql="insert into parkingbook(email,password,car_number,entry,exittime,slots,rat
 				{
 					echo "error ".$sql."".$con->error;
 				}
-				$check=$_SESSION['page'];
-				echo $check;
+				
+				
 				if($check=="demo1")
 				{
 					$sql="update demo1 set status='unavailable',user='$c' where slot='$f'";
