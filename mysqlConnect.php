5f65cfa4208e0b3414cb13f47605054d84cc7729
devvrat02 <34860774+devvrat02@users.noreply.github.com>
2020-01-31 22:51:03 +0530

    comment

--- mysqlConnect.php
+++ mysqlConnect.php
@@ -1,10 +1,18 @@
 <?php
 
-$servername="bsdyxgcmc1ij1rsxuiae-mysql.services.clever-cloud.com";
-$username="ujj3ghjqtx4qdtwy";
-$password="fXbl4su78rBnom0pfeWM";
-$dbname="bsdyxgcmc1ij1rsxuiae";
-		$con=new mysqli($servername,$username,$password,$dbname);
+           /*
+                            *
+                            * Project Name: 	E-parking Management
+                            * Author List: 		Chetan Malviya
+                            * Filename: 		mysqlConnect.php
+                            *
+            */
+
+$servername="bsdyxgcmc1ij1rsxuiae-mysql.services.clever-cloud.com"; //sql server name
+$username="ujj3ghjqtx4qdtwy";						//sql User 	
+$password="fXbl4su78rBnom0pfeWM";					//sql  Password	
+$dbname="bsdyxgcmc1ij1rsxuiae";						//sql database
+		$con=new mysqli($servername,$username,$password,$dbname);	//Connection Establishment 
 		if($con->connect_error)
 		{
 			die("connection failed" .$conn->connect_error);
