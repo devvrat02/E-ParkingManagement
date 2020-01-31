b31d4f080b6cce1a29a61a502315ada7c96dba9b
devvrat02 <34860774+devvrat02@users.noreply.github.com>
2020-01-31 22:49:10 +0530

    comment

--- logout.php
+++ logout.php
@@ -1,4 +1,12 @@
 <?php
+
+           /*
+                            *
+                            * Project Name: 	E-parking Management
+                            * Author List: 		Chetan Malviya
+                            * Filename: 		logout.php
+                            *For closing the connection
+            */
 session_start();
 session_destroy();
 header("location:index.php");
