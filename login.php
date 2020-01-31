2f79263c9e96db397462577a47e80752ae779b36
devvrat02 <34860774+devvrat02@users.noreply.github.com>
2020-01-31 22:47:44 +0530

    comment

--- login.php
+++ login.php
@@ -1,6 +1,14 @@
 <?php
+
+           /*
+                            *
+                            * Project Name: 	E-parking Management
+                            * Author List: 		Chetan Malviya
+                            * Filename: 		login.php
+                            *
+            */
 session_start();
-require 'mysqlConnect.php';
+require 'mysqlConnect.php';//Connection Establishment
 ?>
 <!DOCTYPE html>
 <html class="text-monospace text-nowrap text-capitalize text-dark">
@@ -24,6 +32,7 @@ require 'mysqlConnect.php';
                 <h2 class="text-secondary shadow text-info">Login</h2>
                 <p></p>
             </div>
+<!-- User   Form          -->
             <form class="text-capitalize text-light bg-dark border rounded border-dark shadow-lg" method="post" action="check_login.php" style="width: 323px;height: 315px;padding: 22px;">
                 <div class="form-group"><label for="email">Username/email</label><input class="form-control item" type="email" id="name" name="email" placeholder="Email" ></div>
                 <div class="form-group"><label for="password">Password</label><input class="form-control item" type="password" id="Password" name="password" placeholder="password"></div>
@@ -39,6 +48,7 @@ require 'mysqlConnect.php';
         <div></div>
         <div></div>
     </section>
+<!-- Script     -->
     <script src="assets/js/jquery.min.js"></script>
     <script src="assets/bootstrap/js/bootstrap.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/baguettebox.js/1.10.0/baguetteBox.min.js"></script>
