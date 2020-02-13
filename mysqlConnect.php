d3aa6d3d0feff529d3daeadbc98fca3308d6799c
devvrat02 <34860774+devvrat02@users.noreply.github.com>
2020-02-13 15:54:24 +0530

    connection

--- mysqlConnect.php
+++ mysqlConnect.php
@@ -8,10 +8,10 @@
                             *
             */
 
-$servername="bsdyxgcmc1ij1rsxuiae-mysql.services.clever-cloud.com"; //sql server name
-$username="ujj3ghjqtx4qdtwy";						//sql User 	
-$password="fXbl4su78rBnom0pfeWM";					//sql  Password	
-$dbname="bsdyxgcmc1ij1rsxuiae";						//sql database
+$servername="eparkm.mysql.database.azure.com";						//bsdyxgcmc1ij1rsxuiae-mysql.services.clever-cloud.com"; //sql server name
+$username="chetan@eparkm";						//ujj3ghjqtx4qdtwy";						//sql User 	
+$password="devvrat@01";//fXbl4su78rBnom0pfeWM";					//sql  Password	
+$dbname="epark";		//"bsdyxgcmc1ij1rsxuiae";						//sql database
 		$con=new mysqli($servername,$username,$password,$dbname);	//Connection Establishment 
 		if($con->connect_error)
 		{
