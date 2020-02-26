59d95fbd36db438cae1d37cb24b74ed0e1650922
devvrat02 <34860774+devvrat02@users.noreply.github.com>
2020-02-26 22:46:35 +0530

    complete

--- contact-us.php
+++ contact-us.php
@@ -29,12 +29,12 @@
                 <div class="block-heading">
                     <h2 class="text-info bg-dark shadow pulse animated infinite">Contact Us</h2>
                 </div>
-                <form class="text-monospace text-light bg-dark border-dark shadow" style="height: 489px;">
-                    <div class="form-group"><label>Name</label><input class="form-control" type="text"></div>
-                    <div class="form-group"><label>Subject</label><input class="form-control" type="text"></div>
-                    <div class="form-group"><label>Email</label><input class="form-control" type="email"></div>
-                    <div class="form-group"><label>Message</label><textarea class="form-control"></textarea></div>
-                    <div class="form-group"><button class="btn btn-primary btn-block" type="submit" data-bs-hover-animate="flash">Send</button></div>
+                <form class="text-monospace text-light bg-dark border-dark shadow" style="height: 489px;" method="post" action="contact.php">
+                    <div class="form-group"><label>Name</label><input class="form-control" type="text" name="name"></div>
+                    <div class="form-group"><label>Subject</label><input class="form-control" type="text" name="subject" ></div>
+                    <div class="form-group"><label>Email</label><input class="form-control" type="email" name="email"></div>
+                    <div class="form-group"><label>Message</label><textarea class="form-control" name="msg" ></textarea></div>
+                    <div class="form-group"><button class="btn btn-primary btn-block" type="submit" name="submit" data-bs-hover-animate="flash">Send</button></div>
                 </form>
             </div>
         </section>
