ad008e925fe5f7338c2905e551c368f38656ea3d
Chetan Malviya <malviyachetan02@gmail.com>
2020-03-22 00:02:35 +0530

    d

--- admin/vehicles.php
+++ admin/vehicles.php
@@ -17,6 +17,7 @@ $d=$_SESSION['prk'];
 
 <head>
     <meta charset="utf-8">
+	<meta http-equiv="refresh" content="5" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Admin|Parking</title>
