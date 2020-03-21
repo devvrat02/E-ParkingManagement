e736a3bd0db5d4257b681959116512c368e8e751
Chetan Malviya <malviyachetan02@gmail.com>
2020-03-22 00:01:55 +0530

    d

--- admin/index.php
+++ admin/index.php
@@ -30,6 +30,7 @@ $result1=$con->query($sql);
 
 <head>
     <meta charset="utf-8">
+	<meta http-equiv="refresh" content="5" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Admin|Dashboard</title>
