d37b476c6b53667f2c72bea16b1eabd82f969263
Chetan Malviya <malviyachetan02@gmail.com>
2020-03-23 23:26:54 +0530

    mail lib

--- mail.php
+++ mail.php
@@ -18,8 +18,18 @@ try {
     echo 'Caught exception: '. $e->getMessage() ."\n";
 }
 }
-// $c='<h1>hello</h1>';
-// $a="malviyachetan02@gmail.com";
-// $b="chetan";
-// mailforward($a,$b,$c);
+function ticket($username,$name,$parking,$slot,$intime,$extime,$cost){
+$content="
+    <body style= 'background-color:#343a40; border: 1px solid rgb(11, 12, 12); padding: 10 px;'>
+        <h2 style='color:white;  text-align: center; font-size: 30; font-family: 'Times New Roman', Times, serif;'> E-parking </h2> 
+        <section class='bg-dark border-light clean-block clean-hero' style='color:white; background-color:rgba(9, 162, 255, 0.85); border: 10px solid #898989; padding: 5px; text-align: center'>
+            <div class='text'>
+                <h1 style ='font-size: 45; font-family: cursive ;'>Booking Confirmed!!</h1>
+                <p style='font-size: 25'>Username: $username<br> Parking: $parking<br> Slot: $slot<br> Entry timeTime: $intime <br> Exit Time: $extime<br> Cost: $cost</p>
+            </div>
+        </section>
+    </body>
+";
+     mailforward($username,$name,$content);
+}
 ?>
